c8885fb04aa204e915f96419fa6a162c887fc569
ShinpeiH <dctechsf@gmail.com>
2024-09-17 21:56:45 -0700

    add operate function and verify that it works

--- calculator.js
+++ calculator.js
@@ -14,7 +14,22 @@ const divide = function(num1, num2) {
     return num1 / num2;
 }
 
-console.log(add(2, 4));
-console.log(subtract(4, 2));
-console.log(multiply(2, 4));
-console.log(divide(4, 2));
+// the above functions work
+
+let firstNumber = 0;
+let secondNumber = 0;
+let operator = [ add, subtract, multiply, divide ];
+
+let operate = function(num1, num2, operator) {
+    if (operator == add) {
+        return add(num1, num2);
+    } else if (operator == subtract) {
+        return subtract(num1, num2);
+    } else if (operator == multiply) {
+        return multiply(num1, num2);
+    } else if (operator == divide) {
+        return divide(num1, num2);
+    }
+};
+
+console.log(operate(4, 2, subtract));
