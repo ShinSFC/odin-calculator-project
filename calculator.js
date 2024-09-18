2f2bd25f0d9567934f55704968187f79c0990165
ShinpeiH <dctechsf@gmail.com>
2024-09-17 20:17:28 -0700

    add 4 basic math functions and verify that they work

---
+++ calculator.js
@@ -0,0 +1,20 @@
+const add = function(num1, num2) {
+    return num1 + num2;
+}
+
+const subtract = function(num1, num2) {
+    return num1 - num2;
+}
+
+const multiply = function(num1, num2) {
+    return num1 * num2;
+}
+
+const divide = function(num1, num2) {
+    return num1 / num2;
+}
+
+console.log(add(2, 4));
+console.log(subtract(4, 2));
+console.log(multiply(2, 4));
+console.log(divide(4, 2));
