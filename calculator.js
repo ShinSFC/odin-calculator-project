10def5db6c340a891467c2d2d41b73f5280a1a5c
ShinpeiH <dctechsf@gmail.com>
2024-09-22 00:05:37 -0700

    addition will work and display result

--- calculator.js
+++ calculator.js
@@ -41,6 +41,17 @@ operatorButton.forEach(button => {
     })
 })
 
+// equal button
+const equalButton = document.querySelector("#equal-button");
+
+equalButton.addEventListener('click', () => {   
+    //console.log(num1); // these work
+    //console.log(num2); // these work
+    //console.log(operator); // these work
+    let result = operate(num1, num2, operator);
+    display.textContent = result;
+});
+
 // math functions
 const add = function(num1, num2) {
     return num1 + num2;
@@ -60,13 +71,18 @@ const divide = function(num1, num2) {
 
 // operate function takes 2 numbers and an operator and returns the answer
 let operate = function(num1, num2, operator) {
-    if (operator == add) {
+    num1 = Number(num1);
+    num2 = Number(num2);
+    console.log(num1);
+    console.log(num2);
+    console.log(operator);
+    if (operator == 'add') {
         return add(num1, num2);
-    } else if (operator == subtract) {
+    } else if (operator == 'subtract') {
         return subtract(num1, num2);
-    } else if (operator == multiply) {
+    } else if (operator == 'multiply') {
         return multiply(num1, num2);
-    } else if (operator == divide) {
+    } else if (operator == 'divide') {
         return divide(num1, num2);
     }
 };
