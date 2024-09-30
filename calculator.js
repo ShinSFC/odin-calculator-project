1fba3abaeceaf51894bd6b7b518152dceb54eea0
ShinpeiH <dctechsf@gmail.com>
2024-09-29 23:12:37 -0700

    worked a bunch of changes to make multiple equations work before pressing equal key

--- calculator.js
+++ calculator.js
@@ -5,7 +5,10 @@ const display = document.querySelector("#display");
 let num1 = '';
 let num2 = '';  
 let operator = '';
+let numBtnCnt = 0;
 let opBtnCnt = 0;       // how many times the operator button was pressed
+let eqBtnCnt = 0;       // how many times the equal button was pressed ********************************************************************************
+let result = 0;         // change made here. declared this variable up here instead of in the equal button function ***********
 
 // number buttons function
 // needs to clear display and then display second number when second number is pressed
@@ -13,11 +16,20 @@ const numberButtons = document.querySelectorAll('.number-buttons');
 
 numberButtons.forEach(button => {
     button.addEventListener('click', () => {
-        display.textContent += button.id.slice(3);
-        if (operator == '') {           // until operator is clicked, numbers are concatenated to num1
-            num1 = display.textContent; // first number stored in variable num1
-        } else {
-            num2 = display.textContent; // second number stored in varible num2
+        numBtnCnt++;
+        if (opBtnCnt === 0) {           
+            num1 += button.id.slice(3); 
+            num1 = Number(num1);
+            display.textContent = num1;
+            console.log(`num1 is ${num1}`);
+        } else if (opBtnCnt > 0) { 
+            num2 = '';
+            num2 += button.id.slice(3);
+            num2 = Number(num2);
+            display.textContent = num2;
+            console.log(`num2 is ${num2}`);
+            result = operate(num1, num2, operator);
+            result = result.toString().slice(0, 10);
         }
     });
 });
@@ -30,18 +42,28 @@ clearButton.addEventListener('click', () => {
     num1 = '';
     num2 = '';
     operator = '';
+    opBtnCnt = 0;
+    eqBtnCnt = 0;
+    numBtnCnt = 0;
 });
 
 // function to save operater button pressed into a variable and clear display  
-// needs to switch to num2 instead of clearing display when pressed
 const operatorButton = document.querySelectorAll(".operator-buttons");
 
 operatorButton.forEach(button => {
     button.addEventListener('click', () => {
         operator = button.id.slice(3);
+        console.log(operator);
+        console.log(`typeof num1 is ${typeof num1}`);
+        console.log(`typeof num2 is ${typeof num2}`);
+        if (eqBtnCnt == 0 && typeof num2 === 'number') { 
+            // result = operate(num1, num2, operator); 
+            console.log(`result inside operator function is ${typeof result}: ${result}`);
+            display.textContent = result;
+            num1 = Number(result);
+        }      
         opBtnCnt++;
-        console.log(`operator btn was pressed ${opBtnCnt}`);
-        display.textContent = '';
+        console.log(`opBtnCnt is ${opBtnCnt}`);
     })
 }) 
 
@@ -49,13 +71,16 @@ operatorButton.forEach(button => {
 const equalButton = document.querySelector("#equal-button");
 
 equalButton.addEventListener('click', () => {  
-    console.log(`num1 is ${num1}`);
-    console.log(`num2 is ${num2}`);
-    console.log(`operator is ${operator}`);
-    let result = operate(num1, num2, operator);
+    result = operate(num1, num2, operator);         // made change here to remove keyword let and declare this variable above ***********
+    eqBtnCnt++;
+    console.log(`eqBtnCnt is ${eqBtnCnt}`);
+    console.log(`num1 is ${num1}, num2 is ${num2}`);
     console.log(`result is ${result}`);
     result = result.toString().slice(0, 10);
     display.textContent = result;
+    num1 = '';
+    num2 = '';                                    // change made here. added this counter *******************************************     // added this to verify counter  *************************************************
+    opBtnCnt = 0;                                   // change made here to reset opBtnCnt to 0 ***************************************
 });
 
 // math functions
@@ -72,16 +97,20 @@ const multiply = function(num1, num2) {
 }
 
 const divide = function(num1, num2) {
+    if (num2 == 0) {
+        display.textContent = "Can't / 0!"; 
+        return;
+    }
     return num1 / num2;
 }
 
 // operate function takes 2 numbers and an operator and returns the answer
 let operate = function(num1, num2, operator) {
-    num1 = Number(num1);
-    num2 = Number(num2);
+    // num1 = Number(num1);
+    // num2 = Number(num2);
     // console.log(num1);
     // console.log(num2);
-    console.log(operator);
+    // console.log(operator);
     if (operator == 'add') {
         return add(num1, num2);
     } else if (operator == 'subtract') {
