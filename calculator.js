43580307ed4f991913dd4388af55d2953dd5daf4
ShinpeiH <dctechsf@gmail.com>
2024-09-30 23:19:00 -0700

    add else if statement under operator button so calculator keeps functioning back to back 3 number operations

--- calculator.js
+++ calculator.js
@@ -1,7 +1,7 @@
 // display 
 const display = document.querySelector("#display");
 
-// number input and button count variables
+// variables
 let num1 = '';
 let num2 = '';  
 let operator = '';
@@ -10,7 +10,7 @@ let opBtnCnt = 0;
 let eqBtnCnt = 0;       
 let result = 0;         
 
-// number buttons function
+// number button function
 const numberButtons = document.querySelectorAll('.number-buttons');
 
 numberButtons.forEach(button => {
@@ -26,7 +26,7 @@ numberButtons.forEach(button => {
             num2 += button.id.slice(3);
             num2 = Number(num2);
             display.textContent = num2;
-            console.log(`num2 is ${num2}`); //************num2 gets concatenated when more than 2 numbers in equation  
+            console.log(`num2 is ${num2}`); 
             result = operate(num1, num2, operator);
             result = result.toString().slice(0, 10);
         }
@@ -46,23 +46,26 @@ clearButton.addEventListener('click', () => {
     numBtnCnt = 0;
 });
 
-// function to save operater button pressed into a variable and clear display  
+// operator button function 
 const operatorButton = document.querySelectorAll(".operator-buttons");
 
 operatorButton.forEach(button => {
     button.addEventListener('click', () => {
         operator = button.id.slice(3);
         console.log(operator);
-        console.log(`typeof num1 is ${typeof num1}`);
-        console.log(`typeof num2 is ${typeof num2}`);
+        console.log(`typeof num1 is ${typeof num1}: ${num1}`);
+        console.log(`typeof num2 is ${typeof num2}: ${num2}`);
         if (eqBtnCnt == 0 && typeof num2 === 'number') { 
             console.log(`result inside operator function is ${typeof result}: ${result}`);
             display.textContent = result;
             num1 = Number(result);
             num2 = '';      // added this to prevent concatenation of num2 when more than 2 numbers in equation
-        }      
+        } else if (opBtnCnt > 0) {
+            display.textContent = result;
+            num1 = Number(result);
+            num2 = '';
+        }
         opBtnCnt++;
-        //console.log(`opBtnCnt is ${opBtnCnt}`);
     })
 }) 
 
@@ -77,7 +80,7 @@ equalButton.addEventListener('click', () => {
     console.log(`result is ${result}`);
     result = result.toString().slice(0, 10);
     display.textContent = result;
-    num1 = '';
+    num1 = '';       // changed this so num1 can retain result
     num2 = '';                                    
     opBtnCnt = 0;                                   
 });
@@ -105,11 +108,6 @@ const divide = function(num1, num2) {
 
 // operate function takes 2 numbers and an operator and returns the answer
 let operate = function(num1, num2, operator) {
-    // num1 = Number(num1);
-    // num2 = Number(num2);
-    // console.log(num1);
-    // console.log(num2);
-    // console.log(operator);
     if (operator == 'add') {
         return add(num1, num2);
     } else if (operator == 'subtract') {
